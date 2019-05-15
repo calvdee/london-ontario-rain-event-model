38992fd1638ed17bd0f2fd6419927b6b6b365317
Calvin De Lima <calvindlm@gmail.com>
2019-05-15 10:55:31 -0400

    Added pipeline script for feature data

--- src/app/feature_data.py
+++ src/app/feature_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import argparse
 
 def prepare_weekly_rain_day_counts(source_path: str) -> pd.DataFrame:
   """ Computes the number of rainy days per week. Expects the data at
@@ -9,3 +10,17 @@ def prepare_weekly_rain_day_counts(source_path: str) -> pd.DataFrame:
   rain_counts = rain_days.resample('1W', label='left').size()
 
   return rain_counts
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('input')
+  parser.add_argument('output')
+  args = parser.parse_args()
+
+  print(f'Processing input file {args.input}')
+
+  output = prepare_weekly_rain_day_counts(args.input)
+
+  output.to_csv(args.output, index=True, sep=',')
+
+  print(f'Saved file to {args.output}')
